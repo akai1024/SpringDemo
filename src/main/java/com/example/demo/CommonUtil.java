011ab66c0af1a52830b679fc6241762eed19d259
kai <kai@o2ogt.com>
2019-01-03 15:30:38 +0800

    增加共用函示

--- src/main/java/com/example/demo/CommonUtil.java
+++ src/main/java/com/example/demo/CommonUtil.java
@@ -5,14 +5,32 @@ import java.util.Date;
 
 public class CommonUtil {
 
+	/**
+	 * 是否是空字串
+	 */
+	public static boolean isEmptyString(String str) {
+		if (str == null || str.isEmpty()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * 是否是有效字串
+	 */
+	public static boolean isEffectiveString(String str) {
+		return !isEmptyString(str);
+	}
+
 	public static String dateToSimpleString(Date date) {
-		if(date == null) {
+		if (date == null) {
 			return null;
 		}
-		
+
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
-		
+
 		StringBuilder sBuilder = new StringBuilder();
 		sBuilder.append(cal.get(Calendar.YEAR));
 		sBuilder.append("/");
@@ -27,6 +45,5 @@ public class CommonUtil {
 		sBuilder.append(cal.get(Calendar.SECOND));
 		return sBuilder.toString();
 	}
-	
-	
+
 }
